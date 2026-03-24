023583707f05845d3691a599b1efed0a9757a43d
Dhivya372007 <dhivya372007@gmail.com>
2026-03-24 20:41:37 +0530

    Methods.java

---
+++ Day 5/Codebase/Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+    public static void main(String[] args) {
+        String s="Hello";
+        System.out.println(s);
+        Methods md=new Methods();
+        md.square(2);
+       //square(2);
+    }
+    public void square(int n){
+        System.out.println(n*n);
+    }
+}
