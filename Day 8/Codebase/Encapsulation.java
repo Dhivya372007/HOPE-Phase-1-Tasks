7d8d2df2e44ac6ef0e462bbfdaf75661340720d4
Dhivya372007 <dhivya372007@gmail.com>
2026-03-27 19:09:53 +0530

    Encapsulation.java

---
+++ Day 8/Codebase/Encapsulation.java
@@ -0,0 +1,17 @@
+class Encapsulation {
+    int a;
+    Encapsulation(){
+        a=10;
+    }
+    void display(){
+        System.out.println("Impelementation of Encapsulation "+a);
+    }
+    
+}
+
+public class EncapsulationImplementation {
+    public static void main(String[] args) {
+        Encapsulation e=new Encapsulation();
+        e.display();
+    }
+}
