0936ad9fbfb87311e1263f96f5cf54d8fad39d48
Dhivya372007 <dhivya372007@gmail.com>
2026-03-27 19:08:59 +0530

    Polymorphism.java

---
+++ Day 8/Codebase/Polymorphism.java
@@ -0,0 +1,24 @@
+class Animal{
+    void sound(){
+        System.out.println("All animals have different sound");
+    }
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal{
+    void sound(){
+        System.out.println("Cat meows");
+    }
+}
+
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Animal a;
+        a=new Cat();
+        a.sound();
+    }
+}
